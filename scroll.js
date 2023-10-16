dd1a449e9a5c7a41598af2e054dcdde53bffdefa
JD-MBP <jashansingh29@gmail.com>
2023-10-16 15:46:44 -0400

    day 2

---
+++ scroll.js
@@ -0,0 +1,27 @@
+
+    // When the user scrolls down 50px from the top of the document, resize the header's font size
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 10) {
+        var header = $(".nav-bar");
+        var an1 = $(".main-container");
+        var an2 = $(".text-one-alt");
+        
+        
+        document.getElementById("header").style.fontSize = "1.75vh";
+        header.removeClass('nav-bar') .addClass("nav-bar-txt");
+        an1.removeClass('main-container') .addClass("main-container-alt");
+        an2.removeClass('text-one-alt') .addClass("text-one");
+    } else {
+        var header = $(".nav-bar-txt"); 
+        var an1 = $(".main-container-alt");
+        var an2 = $(".text-one");
+        
+        document.getElementById("header").style.fontSize = "2.5vh";
+        header.removeClass("nav-bar-txt") .addClass('nav-bar'); 
+        an1.removeClass('main-container-alt') .addClass("main-container");
+        an2.removeClass('text-one') .addClass("text-one-alt");
+    }
+    }
+
