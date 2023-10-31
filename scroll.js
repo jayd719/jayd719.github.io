a2bce8c998fb8725fe7b8aa37d3c5df17bf7f644
JD-MBP <jashansingh29@gmail.com>
2023-10-30 23:58:56 -0400

    30oct

--- scroll.js
+++ scroll.js
@@ -7,21 +7,36 @@
         var header = $(".nav-bar");
         var an1 = $(".main-container");
         var an2 = $(".text-one-alt");
-        
-        
+        var an3 = $(".menu-two-alt");
+        var an4 = $(".menu");
         document.getElementById("header").style.fontSize = "1.75vh";
         header.removeClass('nav-bar') .addClass("nav-bar-txt");
         an1.removeClass('main-container') .addClass("main-container-alt");
         an2.removeClass('text-one-alt') .addClass("text-one");
+        an3.removeClass("menu-two-alt").addClass("menu-two");
+        an4.removeClass("menu").addClass("menu-alt")
     } else {
         var header = $(".nav-bar-txt"); 
         var an1 = $(".main-container-alt1");
         var an2 = $(".textone");
+        var an3 = $(".menu-two");
+        var an4 = $(".menu-alt");
         
         document.getElementById("header").style.fontSize = "2.25vh";
         header.removeClass("nav-bar-txt") .addClass('nav-bar'); 
         an1.removeClass('main-container-alt') .addClass("main-container");
         an2.removeClass('text-one') .addClass("text-one-alt");
+        an3.removeClass("menu-two").addClass("menu-two-alt")
+        an4.removeClass("menu-alt").addClass("menu")
     }
+    }
+
+
+    function scollAnimation2(){
+        if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 100){
+            var jackImage = $("jack")
+            window.alert("Ti")
+        }
+
     }
 
