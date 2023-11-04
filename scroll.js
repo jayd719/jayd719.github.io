f9f273d4bfd6a1bba23ff97ffe300813e16180a5
JD-MBP <jashansingh29@gmail.com>
2023-11-04 15:16:57 -0400

    l

--- scroll.js
+++ scroll.js
@@ -10,7 +10,8 @@
         var an3 = $(".menu-two-alt");
         var an4 = $(".menu");
         var an5 = $(".menu-one");
-
+        var ann6 = $(".spiral-image")
+;
         document.getElementById("header").style.fontSize = "1.5vh";
         header.removeClass('nav-bar') .addClass("nav-bar-txt");
         an1.removeClass('main-container') .addClass("main-container-alt");
@@ -18,7 +19,8 @@
         an3.removeClass("menu-two-alt").addClass("menu-two");
         an4.removeClass("menu").addClass("menu-alt")
         an5.removeClass("menu-one").addClass("menu-one-alt")
-        
+        an6.removeClass("spiral-image").addClass("spiral-image-alt")
+
     } else {
         var header = $(".nav-bar-txt"); 
         var an1 = $(".main-container-alt1");
