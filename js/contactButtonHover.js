8ad9d4b98f1f34622bac745d671a4e60de1eab3a
JD-MBP <jashansingh29@gmail.com>
2023-11-19 15:26:11 -0500

    ;

--- js/contactButtonHover.js
+++ js/contactButtonHover.js
@@ -1,11 +1,26 @@
 const contactButton = document.getElementById("contact-button");
-const colorArray = ["#2EC4B6", "#E71D36", "#FF9F1C","#072AC8","#1E96FC","#5C5D8D"];
+let prev = 0;
+function randomColor() {
+  const colorArray = [
+    "#2EC4B6",
+    "#E71D36",
+    "#FF9F1C",
+    "#072AC8",
+    "#1E96FC",
+    "#5C5D8D",
+  ];
+  
+  let no = Math.floor(Math.random() * colorArray.length);
+  while(no ==prev){
+    no = Math.floor(Math.random() * colorArray.length);
+  }
+  prev = no;
+  return colorArray[no];
+}
+
 contactButton.addEventListener("mouseover", () => {
-    let no = Math.floor(Math.random() *  colorArray.length);
-    contactButton.style.backgroundColor=colorArray[no];
+  contactButton.style.backgroundColor = randomColor();
 });
-
-contactButton.addEventListener('mouseleave',()=>{contactButton.style.backgroundColor='#c6ff7d'})
-
-
-
+contactButton.addEventListener("mouseleave", () => {
+  contactButton.style.backgroundColor = "#c6ff7d";
+});
