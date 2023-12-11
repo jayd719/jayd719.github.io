1d67d5f9b3866c1dea117be62748e77332b41707
JD-MBP <jashansingh29@gmail.com>
2023-12-11 13:25:11 -0500

    ;

--- js/circleHover.js
+++ js/circleHover.js
@@ -1,8 +1,22 @@
 let circle = document.getElementById("circle");
+
+let moving = true;
+
 var x = setInterval(function () {
-  let varX = Math.floor(Math.random() * 950) + 100;
-  let varY = Math.floor(Math.random() * 950) + 100;
-  circle.style.top = `${varY}px`;
-  circle.style.left = `${varX}px`;
+  if (moving) {
+    let varX = Math.floor(Math.random() * 950) + 100;
+    let varY = Math.floor(Math.random() * 950) + 100;
+    circle.style.top = `${varY}px`;
+    circle.style.left = `${varX}px`;
+  }
 }, 2000);
 
+circle.addEventListener("mouseenter", () => {
+  moving = false;
+  ChangeCSS(".chatBox-hidden", ".chatBox", true);
+});
+
+circle.addEventListener("mouseleave", () => {
+  moving = true;
+  ChangeCSS(".chatBox", ".chatBox-hidden", true);
+});
