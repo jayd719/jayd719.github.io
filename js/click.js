f86cafe43718d36457f1fed6e61d5fa25f9fed0e
JD-MBP <jashansingh29@gmail.com>
2023-12-19 20:10:36 -0500

    ;

--- js/click.js
+++ js/click.js
@@ -11,14 +11,7 @@ function ChangeCSS(from, to, class_) {
   return value;
 }
 
-function sleep(ms) {
-  var date = new Date();
-  var curDate = null;
 
-  do {
-    curDate = new Date();
-  } while (curDate - date < ms * 1000);
-}
 
 function hideJacks() {
   ChangeCSS(".spiral-image", ".spiral-image-alt", true);
